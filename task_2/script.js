8f6bb35c96859931fde167650968e109901cb35a
Itay Kalfon <iykalfon@gmail.com>
2023-09-02 12:42:07 +0300

    fix: seperated all movement logic from rendering logic

--- task_2/script.js
+++ task_2/script.js
@@ -1,166 +1,170 @@
-$(document).ready(function() {
-  let currentPlayer = 1; // Player 1 starts
-  let playerNames = [];
-  let gameBoard = []; // You can use a 2D array to represent the board state
-  let moves = 0
-  let isPawnClicked = 0
-  const numRows = 8;
-  const numCols = 8;
+var currentPlayer = 1; // Player 1 starts
+var playerNames = [];
+var gameBoard = []; // You can use a 2D array to represent the board state
+var moves = 0
+var isPawnClicked = 0
+const numRows = 8;
+const numCols = 8;
 
-  // Function to initialize the game board and pieces
-  function initializeBoard() {
-    const board = $('.board');
-  
-    // Create the game board array with initial piece positions
-    for (let row = 0; row < numRows; row++) {
-      gameBoard[row] = [];
-      for (let col = 0; col < numCols; col++) {
-        if ((row + col) % 2 === 1) { // Add pieces only on dark squares
-          if (row < 3) {
-            gameBoard[row][col] = 1; // Player 1's piece
-          } else if (row > 4) {
-            gameBoard[row][col] = 2; // Player 2's piece
-          } else {
-            gameBoard[row][col] = 0; // Empty square
-          }
+function initializeBoard() {
+  gameBoard = []; 
+  // Create the game board array with initial piece positions
+  for (let row = 0; row < numRows; row++) {
+    gameBoard[row] = [];
+    for (let col = 0; col < numCols; col++) {
+      if ((row + col) % 2 === 1) { // Add pieces only on dark squares
+        if (row < 3) {
+          gameBoard[row][col] = 1; // Player 1's piece
+        } else if (row > 4) {
+          gameBoard[row][col] = 2; // Player 2's piece
         } else {
-          gameBoard[row][col] = -1
+          gameBoard[row][col] = 0; // Empty square
         }
-        // Create a square element and append it to the board
-        const tile = $('<div>').addClass('tile');
-        if (gameBoard[row][col] === 1) {
-          tile.addClass('black-tile');
-          const pawn = $('<div>').addClass('pawn');
-          pawn.addClass('pawn-p1')
-          tile.append(pawn);
-        } else if (gameBoard[row][col] === 2) {
-          tile.addClass('black-tile');
-          const pawn = $('<div>').addClass('pawn');
-          pawn.addClass('pawn-p2')
-          tile.append(pawn);
-        } else if (gameBoard[row][col] === 0) {
-          tile.addClass('black-tile');
-        } else if (gameBoard[row][col] === -1){
-          tile.addClass('white-tile')
-        }
-        tile.appendTo(board);
-      }
-    }
-  
-    // Add event listeners for each board square to handle moves
-    $('.pawn').click(function() {
-      if (isPawnClicked === 0){
+      } else {
+        gameBoard[row][col] = -1
+      }}
+  }
+}
+
+function pawnAddEventListener(pawn) {
+  pawn.click(function() {
+    if ($(this).hasClass(`pawn-p${currentPlayer}`)){
+      if (isPawnClicked === 0) {
         console.log('clicked')
-        isPawnClicked = 1
+        
         const row = Math.floor($(this).parent().index() / numRows); // Calculate row index
         const col = $(this).parent().index() % numCols; // Calculate column index
-        if ($(this).hasClass('pawn-p' + currentPlayer)){
-          handleMove(row, col, this);
-        }
+        
+        handleMove(row, col);
+        isPawnClicked = 1
       }
       else if (isPawnClicked === 1) {
         $('.tile').removeClass('valid-move')
         isPawnClicked = 0
       }
-    });
-  }
+    }
+  });
+}
 
-  // Function to switch turns between players
-  function switchTurn() {
-    currentPlayer = 3 - currentPlayer; // Toggle between 1 and 2
-    $('.turn').text(`Current Turn: ${playerNames[currentPlayer - 1]}`);
-    moves ++;
-    $('.moves').text(`Number of Moves: ${moves}`);
-    isPawnClicked = 0
-  }
+function renderBoard() {
+  board = $('.board')
+  board.empty()
 
-  // Function to handle a move
-  function handleMove(row,col, choosenPawn) {
-    // Implement move validation, piece movement, capturing, and board update
-    // Check for win conditions
-    // Update the display
-    const selectedPiece = gameBoard[row][col];
-    //console.log(choosenPawn)
-    // Check if the selected square contains the current player's piece
-    if (selectedPiece === currentPlayer) {
-      // Implement your movement logic here
-      // For example, you can highlight possible valid moves for the selected piece
-      // and handle the actual movement when a valid destination square is clicked
+  for (let row = 0; row < numRows; row++) {
+    for (let col = 0; col < numCols; col++) {
+      let currentTile = gameBoard[row][col]
       
-      highlightValidMoves(row, col);
+      let tileId = `r${row}c${col}`
+      let tile = $(`#${tileId}`)
+      let pawn = $('<div>').addClass('pawn');
+
+      pawnAddEventListener(pawn)
+
+      if (tile.length === 0) {
+        tile = $('<div>').addClass('tile');
+        tile.attr('id', `r${row}c${col}`)
+      }
       
-      // Add event listener for valid destination squares
-      $('.valid-move').click(function(e) {
-        e.preventDefault();
-        const destRow = Math.floor($(this).index() / numRows);
-        const destCol = $(this).index() % numCols;
+      switch (currentTile) {
+        case 1:
+        case 2:
+          if (!tile.hasClass('black-tile')) {
+            tile.addClass('black-tile');
+          }
+          pawn.addClass(`pawn-p${currentTile}`)
+          tile.append(pawn);
+          break;
         
-        // Implement the movement logic
-        gameBoard[destRow][destCol] = selectedPiece;
-        gameBoard[row][col] = 0; // Clear the source square
-        //console.log(gameBoard);
-        
-        // Update the display
-        $(this).append(choosenPawn);
-        $('.valid-move').removeClass('valid-move');
-        
-        // Check for capturing and other game rules
-        // Switch turns to the next player
-        switchTurn();
-        
-      });
+        case 0:
+          if (!tile.hasClass('black-tile')) {
+            tile.addClass('black-tile');
+          }
+          break;
+
+        case -1:
+          if (!tile.hasClass('white-tile')) {
+            tile.addClass('white-tile');
+          }
+          break;
+      }
+      tile.appendTo(board);
     }
   }
+}
 
-  // Function to highlight valid moves for a selected piece
-  function highlightValidMoves(row, col) {
-    const selectedPiece = gameBoard[row][col];
+// Function to switch turns between players
+function switchTurn() {
+  currentPlayer = 3 - currentPlayer; // Toggle between 1 and 2
+  $('.turn').text(`Current Turn: ${playerNames[currentPlayer - 1]}`);
+  moves ++;
+  $('.moves').text(`Number of Moves: ${moves}`);
+  isPawnClicked = 0
+}
 
-    // Clear existing valid move highlights
-    $('.valid-move').removeClass('valid-move');
+// Function to handle a move
+function handleMove(row, col) {
+  const selectedPiece = gameBoard[row][col];
 
-    // Define possible move directions based on the player
-    const moveDirections = (selectedPiece === 2) ? [[-1, -1], [-1, 1]] : [[1, -1], [1, 1]];
-    // Check each move direction
-    for (const direction of moveDirections) {
-      const newRow = row + direction[0];
-      const newCol = col + direction[1];
+  if (selectedPiece === currentPlayer) {
+    
+    highlightValidMoves(row, col);
+    
+    // Add event listener for valid destination squares
+    $('.valid-move').click(function(e) {
+      e.preventDefault();
+      const destRow = Math.floor($(this).index() / numRows);
+      const destCol = $(this).index() % numCols;
 
-      // Check if the new position is within the bounds of the board
-      if (newRow >= 0 && newRow < numRows && newCol >= 0 && newCol < numCols) {
-        const targetPiece = gameBoard[newRow][newCol];
-        // Check if the target square is empty
-        if (targetPiece === 0) {
-          $(`.tile:eq(${newRow * numCols + newCol})`).addClass('valid-move');
-        }
+      // Implement the movement logic
+      // console.log(gameBoard);
+      if (gameBoard[destRow][destCol] !== -1 ) {
+        gameBoard[destRow][destCol] = selectedPiece;
+        gameBoard[row][col] = 0; // Clear the source square
+        $('.valid-move').removeClass('valid-move');
+        switchTurn();
+      }
+      renderBoard();
+    });
+  }
+}
+
+// Function to highlight valid moves for a selected piece
+function highlightValidMoves(row, col) {
+  const selectedPiece = gameBoard[row][col];
+
+  // Clear existing valid move highlights
+  $('.valid-move').removeClass('valid-move');
+
+  // Define possible move directions based on the player
+  const moveDirections = (selectedPiece === 2) ? [[-1, -1], [-1, 1]] : [[1, -1], [1, 1]];
+  // Check each move direction
+  for (const direction of moveDirections) {
+    const newRow = row + direction[0];
+    const newCol = col + direction[1];
+
+    // Check if the new position is within the bounds of the board
+    if (newRow >= 0 && newRow < numRows && newCol >= 0 && newCol < numCols) {
+      const targetPiece = gameBoard[newRow][newCol];
+      // Check if the target square is empty
+      if (targetPiece === 0) {
+        $(`#r${newRow}c${newCol}.tile`).addClass('valid-move');
       }
     }
   }
+}
+
+$(document).ready(function() {
+  
   // Event listener for form submission
   $('#nameForm').submit(function(event) {
     event.preventDefault();
     playerNames.push($('#player1').val(), $('#player2').val());
     $('#nameForm').hide();
     initializeBoard();
+    renderBoard();
     $('.turn').text(`Current Turn: ${playerNames[currentPlayer - 1]}`);
     $('.moves').text('Number of Moves: ' + moves)
   });
 
-  // Event listener for board squares
-  $('.board').on('click', '.tile', function() {
-    // Handle the move when a square is clicked
-    // Use handleMove() function
-  });
-
-  // Event listener for restart button
-  $('.restart-button').click(function() {
-    // Reset the game state and UI
-    $('.board').empty();
-    $('#nameForm').show();
-    currentPlayer = 1
-    moves = 0
-    $('.turn').text(`Current Turn: ${playerNames[currentPlayer - 1]}`);
-    $('.moves').text(`Number of Moves: ${moves}`);
-  });
+  // Add event listeners for each board square to handle moves
 });
-
